7fdcdb39ba572699da949e0edbbde7f09c9cab55
Martin Stottmeister <mstottmeister@mmm.com>
2020-07-29 15:02:59 +0200

    game

---
+++ car_game/car-game.py
@@ -0,0 +1,73 @@
+# car test
+
+
+import pin
+import time
+import sys, pygame
+from  pygame.locals import *
+
+pygame.init()
+
+ena=0
+enb=0
+
+pin.load("config.json")
+
+size = width, height =600,400
+screen = pygame.display.set_mode(size)
+
+while True:
+    time.sleep(0.1)
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            pin.Level("ENA",0)
+            pin.Level("ENB",0)
+            pin.cleanup()
+            pygame.display.quit()
+            sys.exit
+        elif event.type == KEYDOWN and event.key == K_w: # foreward
+            print("w - foreward")
+            ena +=10
+            enb +=10
+        elif event.type == KEYDOWN and event.key == K_s: # backward
+            print("s - backward")
+            ena -=10
+            enb -=10
+        elif event.type == KEYDOWN and event.key == K_a: # left
+            print("a-left")
+            ena +=5
+            enb -=5
+        elif event.type == KEYDOWN and event.key == K_d: # right
+            print("d-right")
+            ena -=5
+            enb +=5
+        elif event.type == KEYDOWN:
+            ena=0
+            enb=0
+
+        # set the motor speed    
+        if(ena>=0):
+            ena = min(ena, 100)
+            pin.Level("ENA",ena)
+            pin.Out("IN1",0)
+            pin.Out("IN2",1)
+        else:
+            ena_minus = abs(ena) 
+            ena_minus = min(ena_minus, 100)
+            pin.Level("ENA",ena_minus)
+            pin.Out("IN1",1)
+            pin.Out("IN2",0)
+        if(enb>=0):
+            enb = min(enb, 100)
+            pin.Level("ENB",enb)
+            pin.Out("IN3",1)
+            pin.Out("IN4",0)
+        else:
+            enb_minus = abs(enb) 
+            enb_minus = min(enb_minus, 100)
+            pin.Level("ENB",enb_minus)
+            pin.Out("IN3",0)
+            pin.Out("IN4",1)
+       
+
+
