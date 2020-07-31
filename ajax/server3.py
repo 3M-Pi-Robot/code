7041f4498652887a48fdd80f48bc787dee4bba15
Martin Stottmeister <mstottmeister@mmm.com>
2020-07-31 07:24:28 +0200

    ajax + bugfix

---
+++ ajax/server3.py
@@ -0,0 +1,48 @@
+import pin
+from flask import Flask
+app = Flask(__name__)
+
+pin.load("config.json")
+
+filename="control.html"
+
+@app.route("/")
+def showIndex():
+    with open(filename, "r") as file:
+        return file.read()
+
+@app.route("/move/<left>/<right>")
+def moveTo(left,right):
+    print(left,right)
+    ena=int(left)
+    enb=int(right)
+
+    # set the motor speed    
+    if(ena>=0):            
+        ena = min(ena, 100)
+        pin.Level("ENA",ena)
+        pin.Out("IN1",0)
+        pin.Out("IN2",1)
+    else:
+        ena_minus = abs(ena) 
+        ena_minus = min(ena_minus, 100)
+        pin.Level("ENA",ena_minus)
+        pin.Out("IN1",1)
+        pin.Out("IN2",0)
+    if(enb>=0):
+        enb = min(enb, 100)
+        pin.Level("ENB",enb)
+        pin.Out("IN3",1)
+        pin.Out("IN4",0)
+    else:
+        enb_minus = abs(enb)        
+        enb_minus = min(enb_minus, 100)
+        pin.Level("ENB",enb_minus)
+        pin.Out("IN3",0)
+        pin.Out("IN4",1)    
+    
+    # return less!
+    return ""
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
